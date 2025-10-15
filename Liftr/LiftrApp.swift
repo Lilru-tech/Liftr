5209bdf4ac5ca13edee42dd45c06f755647fb33a
Lilru-tech <d.g.sanc@gmail.com>
2025-10-15 10:15:35 +0200

    Initial Commit

---
+++ Liftr/LiftrApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftrApp.swift
+//  Liftr
+//
+//  Created by David Gomez sanchez on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LiftrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiftrTests/LiftrTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftrTests.swift
+//  LiftrTests
+//
+//  Created by David Gomez sanchez on 15/10/25.
+//
+
+import Testing
+@testable import Liftr
+
+struct LiftrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
