000ed2f4fcdac3a7792c214dbb84ddb65587ab9c
David Gómez <d.g.sanc@gmail.com>
2026-04-23 12:21:42 +0200

    added dynamic island and lock screen widgets

---
+++ LiftrWorkoutLiveActivity/LiftrWorkoutLiveActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  LiftrWorkoutLiveActivityBundle.swift
+//  LiftrWorkoutLiveActivity
+//
+//  Created by David Gomez sanchez on 23/4/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LiftrWorkoutLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        LiftrWorkoutLiveActivity()
+        LiftrWorkoutLiveActivityControl()
+        LiftrWorkoutLiveActivityLiveActivity()
+    }
+}
